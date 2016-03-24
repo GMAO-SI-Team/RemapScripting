02c77ebd061a6f76c302d75de6d6a28f4c131d39
Matthew Thompson <matthew.thompson@nasa.gov>
2016-03-24 10:46:22 -0400

    MAT: Edits to run_full_remap

--- run_full_remap.bash
+++ run_full_remap.bash
@@ -7,45 +7,70 @@ die () {
    exit 1
 }
 
-if [[ $# != 1 ]]
+if [[ $# != 2 ]]
 then
-   die "1 argument required (num levels), $# provided"
+   die "2 arguments required (outdir, numlevels), $# provided"
 fi
 
-NUMLEVELS=$1
+OUTDIR=$1
+
+NUMLEVELS=$2
 
 LEVDIR=L${NUMLEVELS}
 
-WORKDIR=$(pwd)
+CURRDIR=$(pwd)
 
 # AEROCOMDIR
 # ----------
 
-AEROCOMDIR=$WORKDIR/fvInput/AeroCom/
+CURRAEROCOMDIR=$CURRDIR/fvInput/AeroCom/
+AEROCOMDIR=$OUTDIR/fvInput/AeroCom/
+
 mkdir -v -p $AEROCOMDIR/$LEVDIR
-cp -v $AEROCOMDIR/scripts/dore
+cp -v $CURRAEROCOMDIR/scripts/doremap $AEROCOMDIR/$LEVDIR/
+
 mkdir -v -p $AEROCOMDIR/$LEVDIR/aero_clm
+cp -v $CURRAEROCOMDIR/scripts/aero_clm/doremap $AEROCOMDIR/$LEVDIR/aero_clm
+
+cd $OUTDIR/fvInput
+ln -sv AeroCom PIESA
+cd $CURRDIR
 
 # CMIPDIR
 # -------
 
-CMIPDIR=$WORKDIR/fvInput/CMIP/
+CURRCMIPDIR=$CURRDIR/fvInput/CMIP/
+CMIPDIR=$OUTDIR/fvInput/CMIP/
+
 mkdir -v -p $CMIPDIR/$LEVDIR
+cp -v $CURRCMIPDIR/scripts/doremap $CMIPDIR/$LEVDIR/
 
 # MERRA2DIR
 # ---------
 
-MERRA2DIR=$WORKDIR/fvInput/MERRA2/
+CURRMERRA2DIR=$CURRDIR/fvInput/MERRA2/
+MERRA2DIR=$OUTDIR/fvInput/MERRA2/
+
 mkdir -v -p $MERRA2DIR/$LEVDIR
+cp -v $CURRMERRA2DIR/scripts/doremap $MERRA2DIR/$LEVDIR/
 
 # NRDIR
 # -----
 
-NRDIR=$WORKDIR/fvInput/NR/
+CURRNRDIR=$CURRDIR/fvInput/NR/
+NRDIR=$OUTDIR/fvInput/NR/
+
 mkdir -v -p $NRDIR/$LEVDIR
+cp -v $CURRNRDIR/scripts/doremap $NRDIR/$LEVDIR/
 
 # CHEMDIR
 # -------
-CHEMDIR=$WORKDIR/fvInput_nc3/g5chem/
+CURRCHEMDIR=$CURRDIR/fvInput_nc3/g5chem/
+CHEMDIR=$OUTDIR/fvInput_nc3/g5chem/
+
 mkdir -v -p $CHEMDIR/$LEVDIR
 mkdir -v -p $CHEMDIR/$LEVDIR/aero_clm
+
+cp -v $CURRCHEMDIR/scripts/aero_clm/README $CHEMDIR/$LEVDIR/aero_clm/
+cp -v $CURRCHEMDIR/scripts/aero_clm/remap $CHEMDIR/$LEVDIR/aero_clm/
+cp -v $CURRCHEMDIR/scripts/aero_clm/remap_gfed $CHEMDIR/$LEVDIR/aero_clm/
