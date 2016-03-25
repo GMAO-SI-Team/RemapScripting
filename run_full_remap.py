683368c4b6b2c39fb3f675bc0fe3d4fd29536006
Matthew Thompson <matthew.thompson@nasa.gov>
2016-03-25 11:51:47 -0400

    MAT: add overwrite option

--- run_full_remap.py
+++ run_full_remap.py
@@ -7,6 +7,11 @@ import argparse
 import subprocess as sp
 
 def rename_dirs(directory, in_pattern, out_pattern):
+    """
+    This function takes a directory and will rename any
+    subdirectories called 'in_pattern' to be named
+    'out_pattern'
+    """
 
     for root, dirs, file in os.walk(directory, topdown=False):
         for dir in dirs:
@@ -16,8 +21,9 @@ def rename_dirs(directory, in_pattern, out_pattern):
 def main():
     comm_args = parse_args()
 
-    numlevs = comm_args['levs']
-    outdir = comm_args['output']
+    numlevs   = comm_args['levs']
+    outdir    = comm_args['output']
+    overwrite = comm_args['overwrite']
 
     currdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -25,12 +31,19 @@ def main():
     print "Current Directory: %s" % currdir
     print "Output Directory: %s" % outdir
 
+    levdir = 'L'+str(numlevs)
+
+    if overwrite:
+        shutil.rmtree(outdir)
+
+    # Copy the current directory tree to outdir,
+    # ignoring any bash, py, or git files
     shutil.copytree(currdir, outdir, symlinks=True, 
             ignore=shutil.ignore_patterns('*.bash', '*.py', '.git*'))
 
-    levdir = 'L'+str(numlevs)
-
+    # Rename the scripts directorys to L<numlevs>
     rename_dirs(outdir, 'scripts', levdir)
+
 
 def parse_args():
 
@@ -38,6 +51,7 @@ def parse_args():
 
     p.add_argument('-l','--levs', type=int, help="Number of levels", required=True)
     p.add_argument('-o','--output', type=str, help="Output directory", required=True)
+    p.add_argument(     '--overwrite', help="Remove output directory", action='store_true')
 
     return vars(p.parse_args())
 
